86a4978a22ffa9b2b4e91fc2bb8616fcadc79860
Rafaeldfb <dfbrafa@hotmail.com>
2023-06-12 17:32:34 -0300

    Cross Browsers improvements for add / close form btn. now uses svg.

--- TodoList/src/components/atoms/FloatingButton.jsx
+++ TodoList/src/components/atoms/FloatingButton.jsx
@@ -1,5 +1,7 @@
 import { useFormActiveContext, useFormActiveUpdateContext } from "../FormContextProvider"
+import Plus from "./CrossSvg"
 import classes from "./floatingButton.module.sass"
+
 
 function FloatingButton() {
   const activeForm = useFormActiveContext();
@@ -11,7 +13,7 @@ function FloatingButton() {
         onClick={activeFormUpdate} 
         title={activeForm ? 'Close' : 'Add a new To Do Activity'}
       >
-        {activeForm ? 'x' : '+'}
+        <Plus size="16px" rotate={activeForm ? '45' : '0'} />
       </button>
     </div>
   )
