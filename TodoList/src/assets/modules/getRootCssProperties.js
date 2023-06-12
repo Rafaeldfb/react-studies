e36721252c2fd89a69d49c0e03c1de5c20f73372
Rafaeldfb <dfbrafa@hotmail.com>
2023-06-12 18:27:34 -0300

    JSDocblock addding

--- TodoList/src/assets/modules/getRootCssProperties.js
+++ TodoList/src/assets/modules/getRootCssProperties.js
@@ -1,3 +1,8 @@
+/**
+ * Returns the property value stored at HTML :root styles
+ * @param {String} propertyName The property name to search at :root styles.
+ * @returns 
+ */
 function getRootProperty(propertyName) {
   const root = document.querySelector('html:root')
   return window.getComputedStyle(root).getPropertyValue(typeof propertyName == 'string' ? propertyName : `${propertyName}`)
